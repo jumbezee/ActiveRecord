73e9a07dfff9c398e17be3bdfbdc3517deb1be6a
Николай Кузьменко < >
2016-06-30 02:33:05 +0300

    fsx "S" bug

--- db/schema.rb
+++ db/schema.rb
@@ -19,4 +19,14 @@ ActiveRecord::Schema.define(version: 20160629225600) do
     t.datetime "updated_at"
   end
 
+  create_table "clients", force: :cascade do |t|
+    t.text     "name"
+    t.text     "phone"
+    t.text     "datestamp"
+    t.text     "barber"
+    t.text     "color"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
 end
