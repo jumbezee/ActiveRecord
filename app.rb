204cf3cd5c73f7ea66ccf973a1afae93d0ebaf8a
Николай Кузьменко < >
2016-06-30 03:53:06 +0300

    keep values on post

--- app.rb
+++ app.rb
@@ -26,6 +26,7 @@ get '/' do
 end
 
 get '/visit' do
+	@c = Client.new
 	@tittle = "Записаться"                                                      
 	@message = "Здесь вы можете записаться"
 	erb :visit			
@@ -33,11 +34,11 @@ end
 
 post '/visit' do                                                                        
    	
-   	c = Client.new params[:client]
-   	if c.save
+   	@c = Client.new params[:client]
+   	if @c.save
    		erb "<h2>Спасибо,вы записались</h2>"
 	else
-		@error = c.errors.full_messages.first
+		@error = @c.errors.full_messages.first
 		erb :visit
 	end                                                                             
 end 
